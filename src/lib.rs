b725238c14274532fe01d80a585af7d1a893dc9b
printfn <printfn@users.noreply.github.com>
2020-02-29 20:39:59 +1300

    Bump version to 0.1.1

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![no_std]
 #![forbid(unsafe_code)]
 #![forbid(dead_code)]
-#![doc(html_root_url = "https://docs.rs/unicode/0.1.0")]
+#![doc(html_root_url = "https://docs.rs/unicode/0.1.1")]
 
 pub(crate) mod codepoint;
 pub mod codepoint_type;

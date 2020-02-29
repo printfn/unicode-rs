7d4fcf9f336dbe7846340c1ce7c22210fd06f20e
printfn <printfn@users.noreply.github.com>
2020-02-29 20:39:24 +1300

    Implement FromStr for GeneralCategory

--- src/general_category.rs
+++ src/general_category.rs
@@ -1,3 +1,5 @@
+use core::str::FromStr;
+
 /// Values for the General_Category (gc) character property.
 /// These values are fixed; no new values will be added.
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
@@ -136,6 +138,46 @@ impl GeneralCategory {
             GeneralCategory::Cs => "Other, surrogate",
             GeneralCategory::Co => "Other, private use",
             GeneralCategory::Cn => "Other, not assigned",
+        }
+    }
+}
+
+impl FromStr for GeneralCategory {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<GeneralCategory, ()> {
+        match s {
+            "Lu" => Ok(GeneralCategory::Lu),
+            "Ll" => Ok(GeneralCategory::Ll),
+            "Lt" => Ok(GeneralCategory::Lt),
+            "Lm" => Ok(GeneralCategory::Lm),
+            "Lo" => Ok(GeneralCategory::Lo),
+            "Mn" => Ok(GeneralCategory::Mn),
+            "Mc" => Ok(GeneralCategory::Mc),
+            "Me" => Ok(GeneralCategory::Me),
+            "Nd" => Ok(GeneralCategory::Nd),
+            "Nl" => Ok(GeneralCategory::Nl),
+            "No" => Ok(GeneralCategory::No),
+            "Pc" => Ok(GeneralCategory::Pc),
+            "Pd" => Ok(GeneralCategory::Pd),
+            "Ps" => Ok(GeneralCategory::Ps),
+            "Pe" => Ok(GeneralCategory::Pe),
+            "Pi" => Ok(GeneralCategory::Pi),
+            "Pf" => Ok(GeneralCategory::Pf),
+            "Po" => Ok(GeneralCategory::Po),
+            "Sm" => Ok(GeneralCategory::Sm),
+            "Sc" => Ok(GeneralCategory::Sc),
+            "Sk" => Ok(GeneralCategory::Sk),
+            "So" => Ok(GeneralCategory::So),
+            "Zs" => Ok(GeneralCategory::Zs),
+            "Zl" => Ok(GeneralCategory::Zl),
+            "Zp" => Ok(GeneralCategory::Zp),
+            "Cc" => Ok(GeneralCategory::Cc),
+            "Cf" => Ok(GeneralCategory::Cf),
+            "Cs" => Ok(GeneralCategory::Cs),
+            "Co" => Ok(GeneralCategory::Co),
+            "Cn" => Ok(GeneralCategory::Cn),
+            _ => Err(()),
         }
     }
 }
